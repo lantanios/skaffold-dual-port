1a1c17b483a4e8886b8b264af2d4bbc2161fc899
lantanios <lantareg@gmail.com>
2021-01-30 20:02:46 -0500

    one docker image, two deployments, diff ports

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = process.env.test == 1 ? 3001 : 3002;
+
+console.log("@#-- -port--", port);
+console.log("@#---process.env.test--", process.env.test);
+
+app.get("/", (req, res) => res.send("Hello World! port: " + port));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
