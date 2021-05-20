77d9dbbeacfa45980b42cd28809d92b358a2051f
Lantanios <lantareg@gmail.com>
2021-05-19 20:35:15 -0400

    added internal service

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,31 @@
+const axios = require("axios");
 const express = require("express");
 const app = express();
+
 const port = process.env.test == 1 ? 3001 : 3002;
 
-console.log("@#-- -port--", port);
-console.log("@#---process.env.test--", process.env.test);
+const serverName = port === 3001 ? "Server-1" : "Server-2";
 
-app.get("/", (req, res) => res.send("Hi from gcloud! port: " + port));
+const INTERNAL_SERVICE_ADDRESS = "http://localhost:5000/";
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const callInternalServer = async (callfrom) => {
+  return await axios
+    .post(INTERNAL_SERVICE_ADDRESS, { caller: callfrom })
+    .catch((error) => {
+      console.log(`${serverName} Error: ${error.message}.`);
+    });
+};
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const internalServerResp = await callInternalServer(serverName);
+  console.log(`${serverName} got request from internet.`);
+
+  res.send(`Response from ${serverName}: "${internalServerResp.data}".`);
+});
+
+app.listen(port, () =>
+  console.log(`${serverName} is listening on port ${port}!`)
+);
